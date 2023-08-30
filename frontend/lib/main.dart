f046a9a34533fcb30a1817e07d2fbf88bcfe2ea6
SandaruwanWGMA <molinduachintha24@gmail.com>
2023-08-30 19:20:39 +0530

    updated the main page

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,16 +1,35 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
   @override
-  Widget build(context) {
-    return const MaterialApp(
-      home: Scaffold(),
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shop-n-Deliver Essentials', // Title of the app
+      theme: ThemeData(
+        primarySwatch: Colors.blue, // App's primary color theme
+      ),
+      home: HomePage(), // Initial screen is set to HomePage
+    );
+  }
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Shop-n-Deliver Essentials'), // Title in the app bar
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to Shop-n-Deliver Essentials!', // Welcome message
+          style: TextStyle(fontSize: 24),
+        ),
+      ),
     );
   }
 }
